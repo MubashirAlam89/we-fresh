a4f766b56484f9fd2771570d3005672a066b6d09
mubashiralam89 <mubashiralam89@gmail.com>
2023-08-07 11:40:24 +0500

    added an onchange event and value on input element

--- src/components/common-componenets/input.tsx
+++ src/components/common-componenets/input.tsx
@@ -4,15 +4,17 @@ import React from "react";
 
 interface props {
   placeholder: string;
-  // type: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  inpValue: string;
 }
 
-export const Input: NextPage<props> = ({ placeholder }) => {
+export const Input: NextPage<props> = ({ placeholder, onChange, inpValue }) => {
   return (
     <input
-      // type={type}
       type="text"
       placeholder={placeholder}
+      onChange={onChange}
+      value={inpValue}
       className="focus:outline-none text-black rounded-md px-4 py-2 max-md:text-lg max-md:py-1 text-xl  w-full border-2  border-black/20"
     />
   );
