0a8955596403c560010ac22796e629fc95aad834
mubashiralam89 <mubashiralam89@gmail.com>
2023-08-10 15:27:19 +0500

    footer modified

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export const Footer = ({
-  websiteLogos,
-  footerSeviceArea,
-  footerAbout,
-  footerHelp,
-  copyright,
-  footer,
-}: any) => {
+export const Footer = ({ websiteLogos, copyright, footer }: any) => {
   return (
     <div className="bg-[#F2F9FF] text-xl text-gray-500">
       <div className="max-w-[1200px] mx-auto px-10 max-md:px-5 py-20 grid grid-cols-5 grid-rows-1 max-md:flex max-md:flex-col gap-5 ">
@@ -20,85 +14,26 @@ export const Footer = ({
             width={200}
           />
         </div>
-        {footer.map((e, i) => {
+        {footer.map((e: any, i: number) => {
           return (
-            <div>
+            <div key={i}>
               <h2 className="font-semibold text-black">{e.fields.heading}</h2>
               <ul className="flex flex-col gap-2 mt-4">
-                {e.fields.footerLink.map((b: any, v: number) => {
+                {e.fields.footerLink.map((el: any, ind: number) => {
                   return (
-                    <li
-                      key={v}
+                    <Link
+                      href={`/${el.fields.url}`}
+                      key={ind}
                       className="cursor-pointer hover:opacity-80 max-md:hover:opacity-100 transition-all"
                     >
-                      {b.fields.link}
-                    </li>
+                      {el.fields.link}
+                    </Link>
                   );
                 })}
               </ul>
             </div>
           );
         })}
-        {/* <div>
-          <h2 className="font-semibold text-black">SERVICE AREA</h2>
-          <ul className="flex flex-col gap-2 mt-4">
-            {footerSeviceArea.map((e: any, i: number) => {
-              return (
-                <li
-                  key={i}
-                  className="cursor-pointer hover:opacity-80 max-md:hover:opacity-100 transition-all"
-                >
-                  {e.fields.item}
-                </li>
-              );
-            })}
-          </ul>
-        </div> */}
-        {/* <div>
-          <h2 className="font-semibold text-black">SERVICE AREA</h2>
-          <ul className="flex flex-col gap-2 mt-4">
-            {footerSeviceArea.map((e: any, i: number) => {
-              return (
-                <li
-                  key={i}
-                  className="cursor-pointer hover:opacity-80 max-md:hover:opacity-100 transition-all"
-                >
-                  {e.fields.item}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div>
-          <h2 className="font-semibold text-black">ABOUT</h2>
-          <ul className="flex flex-col gap-2 mt-4">
-            {footerAbout.map((e: any, i: number) => {
-              return (
-                <li
-                  key={i}
-                  className="cursor-pointer hover:opacity-80 max-md:hover:opacity-100 transition-all"
-                >
-                  {e.fields.item}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div>
-          <h2 className="font-semibold text-black">HELP</h2>
-          <ul className="flex flex-col gap-2 mt-4">
-            {footerHelp.map((e: any, i: number) => {
-              return (
-                <li
-                  key={i}
-                  className="cursor-pointer hover:opacity-80 max-md:hover:opacity-100 transition-all"
-                >
-                  {e.fields.item}
-                </li>
-              );
-            })}
-          </ul>
-        </div> */}
         <div>
           <h2 className="text-center">Available on</h2>
           <div className="flex gap-5 justify-center items-center w-fit mx-auto mt-4">
