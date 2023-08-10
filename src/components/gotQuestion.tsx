1d6caaf78488ca56a72e2c2b1366f07be5d05220
mubashiralam89 <mubashiralam89@gmail.com>
2023-08-10 12:40:23 +0500

    shifted all content on contenful

--- src/components/gotQuestion.tsx
+++ src/components/gotQuestion.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import { Accordian } from "./accordian";
 import { _accordianData } from "@/constants/data";
-export const GotQuestions = () => {
-  const [accordianData, setAccordianData] =
-    useState<{ question: string; answer: string; open: boolean }[]>(
-      _accordianData
-    );
+export const GotQuestions = ({ GotQuestionsSection, faqs }: any) => {
+  const [accordianData, setAccordianData] = useState<any>(faqs);
+  // const [accordianData, setAccordianData] =
+  //   useState<{ question: string; answer: string; open: boolean }[]>(
+  //     _accordianData
+  //   );
 
   const toggleAccordian = (index: number) => {
     setAccordianData(
-      accordianData.map((acrdn, i) => {
+      accordianData.map((acrdn: any, i: number) => {
         if (i === index) {
-          acrdn.open = !acrdn.open;
+          acrdn.fields.isOpen = !acrdn.fields.isOpen;
         } else {
-          acrdn.open = false;
+          acrdn.fields.isOpen = false;
         }
 
         return acrdn;
@@ -26,15 +27,15 @@ export const GotQuestions = () => {
       <div className="w-3/4 mx-auto max-lg:w-4/5 max-md:w-full">
         <div className="text-center">
           <h1 className="text-5xl font-semibold max-md:text-4xl text-blue-500 mb-4">
-            Got questions?
+            {GotQuestionsSection.title}
           </h1>
-          <p className="text-2xl">Perfect, we've got answers!</p>
+          <p className="text-2xl">{GotQuestionsSection.brief}</p>
         </div>
         <div className="accordianData flex flex-col gap-5 max-md:gap-0 mt-10">
-          {accordianData.map((acrdn, i) => (
+          {accordianData.map((acrdn: any, i: number) => (
             <Accordian
               key={i}
-              acrdn={acrdn}
+              acrdn={acrdn.fields}
               index={i}
               toggleAccordian={toggleAccordian}
             />
