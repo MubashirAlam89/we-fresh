221d9a134d37c2d0f90e37d3c3d86c58362dcaab
mubashiralam89 <mubashiralam89@gmail.com>
2023-08-10 12:40:14 +0500

    shifted all content on contenful

--- src/components/getTheApp.tsx
+++ src/components/getTheApp.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
 
-export const GetTheApp = () => {
+export const GetTheApp = ({ getTheAppSection }: any) => {
   return (
     <div className="bg-[#F2F9FF] py-20  px-10 max-md:px-5">
       <div className="max-w-2xl mx-auto flex flex-col gap-5 text-center">
         <h1 className="text-5xl max-md:text-4xl text-blue-500 ">
-          Ready to <b> Get the app?</b>
+          <b>{getTheAppSection.title} </b>
         </h1>
-        <p className="text-xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel saepe
-          ipsam dolorem quisquam ipsum. Ducimus asperiores quas.
-        </p>
+        <p className="text-xl">{getTheAppSection.brief}</p>
         <div className="grid grid-cols-2 grid-rows-1 w-2/4 mx-auto gap-4">
           <Image
             src={"/playstore.png"}
